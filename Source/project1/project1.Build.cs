95fc6a63d3bcb24b0ad53f5cd3eede98ce72031d
ChoiSeungho <tizkzks96@naver.com>
2022-04-07 16:33:40 +0900

    add "how to use about pak DLC" sample

--- Source/project1/project1.Build.cs
+++ Source/project1/project1.Build.cs
@@ -8,7 +8,7 @@ public class project1 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AdditionalMap" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
